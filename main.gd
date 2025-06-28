479ba9f63f64d7ed96bb8338a34ce2f953945cea
Summers Pittman <secondsun@gmail.com>
2025-06-28 10:09:22 -0400

    Finished tutorial

--- main.gd
+++ main.gd
@@ -3,17 +3,22 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready():
-	new_game()
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+	
 func new_game():
 	score = 0
+	$Music.play()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	get_tree().call_group("mobs", "queue_free")
 
 
 
@@ -44,6 +49,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
